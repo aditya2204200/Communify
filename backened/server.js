ffaf3c3230ab6504329f3592a9b396f6c77b013c
Aditya Raj <ar25200312@gmail.com>
2026-04-02 23:29:16 +0530

    Implement chat data endpoint in server.js
    Added chat data endpoint and commented out single chat endpoint.

--- backened/server.js
+++ backened/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
-const app = express()
+const app = express();
 
 app.get('/', (req,res) => {
     res.send("API is Running");
 })
 
+// app.get('/api/chat/id', (req,res) => {
+//     // console.log(req)
+//     const singleChat = chats.find((c) => c._id === req.params.id);
+//     res.send(singleChat);
+// });
+
+app.get("/api/chat" , (req,res) => {
+    res.send(chats);
+})
+
 app.listen(8000,console.log("Server started on PORT 8000"));
+
 
